66626cb60a9b7e442b3424b145e55dd6be07c98d
saqqdy <saqqdy@users.noreply.github.com>
2023-05-10 18:46:01 +0800

    css

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
 function cssThrottle() {
-	//
+	// .throttle {
+	// 	animation: throttle 2s step-end forwards;
+	// }
+	// .throttle:active {
+	// 	animation: none;
+	// }
+	// @keyframes throttle {
+	// 	from {
+	// 		pointer-events: none;
+	// 		opacity: 0.99;
+	// 	}
+	// 	to {
+	// 		pointer-events: all;
+	// 		opacity: 1;
+	// 	}
+	// }
+	// .throttle{animation:throttle 2s step-end forwards}
+	// .throttle:active{animation:none}
+	// @keyframes throttle{from{pointer-events:none;opacity:.99}to{pointer-events:all;opacity:1}}
 }
 
 export { cssThrottle as default }
